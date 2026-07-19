7d92c9e1335e07c6e48104f76da3787740ae9ce7
RudenkoAD <rudenko.ad@phystech.edu>
2026-07-19 16:02:26 +0300

    add tilesets & level 1

--- UI/controls_subscenes/controls_container.gd
+++ UI/controls_subscenes/controls_container.gd
@@ -4,6 +4,7 @@ class_name ControlsContainer
 @onready var label: Label = $Label
 @onready var button: Button = $Button
 
+@export var display_name: String
 @export var action_name: String
 @export var action_bind: String
 
@@ -11,9 +12,10 @@ signal control_rebound(action_name: String, action_bind: InputEventKey)
 var waiting = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label.text = action_name
+	label.text = display_name
+	action_bind = InputMap.action_get_events(action_name)[0].as_text()
 	button.text = action_bind
-
+	
 
 func _on_button_pressed() -> void:
 	waiting = true
--- UI/controls_subscenes/controls_list.gd
+++ UI/controls_subscenes/controls_list.gd
@@ -6,14 +6,7 @@ const CONTROLS_CONTAINER = preload("res://UI/controls_subscenes/controls_contain
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var actions = InputMap.get_actions().slice(91)
-	for action in actions:
-		if prefix in action:
-			var new_cont: ControlsContainer = CONTROLS_CONTAINER.instantiate()
-			new_cont.action_name = action
-			new_cont.action_bind = InputMap.action_get_events(action)[0].as_text()
-			new_cont.control_rebound.connect(_on_control_rebound)
-			self.add_child(new_cont)
+	pass
 		
 		
 
